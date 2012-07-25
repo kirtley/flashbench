cee3d08ab6a223b1087c5481ac18a83f83436e4e
kirtley <kirtley@GX520>
2012-07-24 21:21:35 -0700

    new file:   run-tests.sh

---
+++ run-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+DRIVE=$1
+for i in {1..5}
+	do
+		hdparm -Tt $DRIVE 
+	done
+for i in {1..5}
+	do
+	BLOCKCOUNT=$[2**$i]
+	BLOCKSIZE=$[$BLOCKCOUNT*512]
+	echo "BLOCKCOUNT = " $BLOCKCOUNT
+	echo "BLOCKSIZE = " $BLOCKSIZE
+	./flashbench -a $DRIVE --blocksize=$BLOCKSIZE
+		for e in 1 2 4 8 16
+			do
+			ERASESIZE=$[$e*1024*1024]
+				for AU in {1..5}
+					do
+					echo "BLOCKSIZE = $BLOCKSIZE ERASESIZE = $ERASESIZE AU = $AU"
+					./flashbench -O --erasesize=$ERASESIZE --blocksize=$BLOCKSIZE $DRIVE  --open-au-nr=$AU
+					done
+			done
+	done
